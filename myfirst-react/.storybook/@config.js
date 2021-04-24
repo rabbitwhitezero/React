c76eae28a6619dc998b7d1469c865948a480b715
rabbitwhitezero <rabbit.white.zero@gmail.com>
2021-04-24 11:11:37 +0900

    storybook added

---
+++ myfirst-react/.storybook/@config.js
@@ -0,0 +1,5 @@
+import {configure} from '@storybook/react';
+
+function loadStroies(){
+    require('../src/story/InputStory');
+}
---
+++ myfirst-react/src/story/InputStory.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import TodayPlan from '../TodayPlan';
+
+storiesOf('TodayPlan',module).add('기본 설정',()=><TodayPlan />);
